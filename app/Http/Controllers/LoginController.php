c8cea35ca54510074bc7032422229dd70abf5303
Atal09 <113996359+Atal09@users.noreply.github.com>
2024-06-04 12:15:10 +0200

    login register
    Hamid carry

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,15 +1,58 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\ChatEntry;
-use App\Models\ChatHistory;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Inertia\Inertia;
-use function Termwind\render;
+use Illuminate\Http\RedirectResponse;
 
 class LoginController extends Controller
 {
-    public function login()
+    public function showLoginForm()
     {
         return Inertia::render('Login');
+    }
+
+    public function login(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'login' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        $login = $request->login;
+        $user = User::where('email',$login)->orWhere('name', $login)->first();
+        if (!$user) {
+            $request->session()->flash('alert', [
+                'type'=>'error',
+                'message'=>'The provided credentials do not match our records.'
+            ]);
+            return back();
+        }
+        if (Auth::attempt(['email' => $user->email, 'password' => $request->password]) ||
+            Auth::attempt(['name' => $user->name, 'password' => $request->password])) {
+            Auth::loginUsingId($user->id);
+            $request->session()->regenerate();
+            return redirect('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+            'username'=>'The provided credentials do not match our records.'
+        ])->onlyInput('email');
+    }
+
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,15 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\ChatEntry;
-use App\Models\ChatHistory;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
-use function Termwind\render;
 
 class RegisterController extends Controller
 {
-    public function register()
+    public function showRegistrationForm()
     {
         return Inertia::render('Register');
+    }
+
+    public function register(Request $request)
+    {
+        /*$validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }*/
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->login($user);
+
+        return redirect('/');
     }
 }
