f2c2b34de8aeef4db8e5ca2fdc2b0323c74bcc6f
RaymondLam18 <1024322@hr.nl>
2024-06-08 14:30:00 +0200

    redirect

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -23,27 +23,29 @@ class LoginController extends Controller
         ]);
 
         $login = $request->login;
-        $user = User::where('email',$login)->orWhere('name', $login)->first();
+        $user = User::where('email', $login)->orWhere('name', $login)->first();
+
         if (!$user) {
             $request->session()->flash('alert', [
-                'type'=>'error',
-                'message'=>'The provided credentials do not match our records.'
+                'type' => 'error',
+                'message' => 'The provided credentials do not match our records.'
             ]);
             return back();
         }
+
         if (Auth::attempt(['email' => $user->email, 'password' => $request->password]) ||
             Auth::attempt(['name' => $user->name, 'password' => $request->password])) {
             Auth::loginUsingId($user->id);
             $request->session()->regenerate();
+            // Redirect naar de hoofdpagina na succesvolle login
             return redirect('/');
         }
 
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
-            'username'=>'The provided credentials do not match our records.'
+            'username' => 'The provided credentials do not match our records.'
         ])->onlyInput('email');
     }
-
 
     public function logout(Request $request)
     {
